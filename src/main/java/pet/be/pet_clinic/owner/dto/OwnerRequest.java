285e090b5388fd1295e5fe9e8626adc77b0cb5c7
cmatos <cmatos@uolinc.com>
2026-05-15 19:28:08 -0300

    [master] add validacao telefone

--- src/main/java/pet/be/pet_clinic/owner/dto/OwnerRequest.java
+++ src/main/java/pet/be/pet_clinic/owner/dto/OwnerRequest.java
@@ -2,13 +2,16 @@ package pet.be.pet_clinic.owner.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
 public record OwnerRequest(
 		@NotBlank String firstName,
 		@NotBlank String lastName,
 		@NotBlank String address,
 		@NotBlank String city,
-		@NotBlank String telephone,
+		@NotBlank
+		@Pattern(regexp = "^\\d{10,11}$", message = "telefone deve conter apenas 10 ou 11 digitos")
+		String telephone,
 		@NotBlank @Email String email
 ) {
 
